b1b2b5b07863842ef48b6690d4745f8f6817e52b
degraffa <ard96@cornell.edu>
2025-12-03 04:09:11 -0500

    Clean up BGFeedAction
    Made the code here more readable :)

--- src/main/java/BoardGame/actions/BGFeedAction.java
+++ src/main/java/BoardGame/actions/BGFeedAction.java
@@ -8,58 +8,32 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 
 public class BGFeedAction extends AbstractGameAction {
 
-    private int magicNumber;
-    private DamageInfo info;
+    private final int strengthGain;
+    private final DamageInfo damageInfo;
 
-    public BGFeedAction(AbstractCreature target, DamageInfo info, int strAmount) {
-        this.info = info;
-        setValues(target, info);
-        this.magicNumber = strAmount;
-        this.actionType = AbstractGameAction.ActionType.DAMAGE;
+    public BGFeedAction(AbstractCreature target, DamageInfo damageInfo, int strengthGain) {
+        this.damageInfo = damageInfo;
+        this.strengthGain = strengthGain;
+        setValues(target, damageInfo);
+        this.actionType = ActionType.DAMAGE;
         this.duration = 0.1F;
     }
 
     public void update() {
         if (this.duration == 0.1F && this.target != null) {
-            AbstractDungeon.effectList.add(
-                new FlashAtkImgEffect(
-                    this.target.hb.cX,
-                    this.target.hb.cY,
-                    AbstractGameAction.AttackEffect.NONE
-                )
-            );
-            this.target.damage(this.info);
+            showAttackEffect();
+            this.target.damage(this.damageInfo);
 
-            //TODO: SlimeBoss should use halfdead state instead of disappearing until the next turn
-            boolean halfDeadCheckPassed = (!this.target.halfDead ||
-                this.target instanceof BGDarkling ||
-                this.target instanceof BGAwakenedOne);
-            if (
-                (((AbstractMonster) this.target).isDying || this.target.currentHealth <= 0) &&
-                halfDeadCheckPassed &&
-                !this.target.hasPower("Minion")
-            ) {
-                // TODO degraffa: Use BG StrengthPower
-                addToBot(
-                    (AbstractGameAction) new ApplyPowerAction(
-                        (AbstractCreature) AbstractDungeon.player,
-                        (AbstractCreature) AbstractDungeon.player,
-                        (AbstractPower) new StrengthPower(
-                            (AbstractCreature) AbstractDungeon.player,
-                            this.magicNumber
-                        ),
-                        this.magicNumber
-                    )
-                );
+            if (isTargetKilled()) {
+                grantStrengthToPlayer();
             }
 
-            if ((AbstractDungeon.getCurrRoom()).monsters.areMonstersBasicallyDead()) {
+            if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
                 AbstractDungeon.actionManager.clearPostCombatActions();
             }
 
@@ -67,5 +41,43 @@ public class BGFeedAction extends AbstractGameAction {
         }
 
         tickDuration();
+    }
+
+    private void showAttackEffect() {
+        AbstractDungeon.effectList.add(
+            new FlashAtkImgEffect(
+                this.target.hb.cX,
+                this.target.hb.cY,
+                AttackEffect.NONE
+            )
+        );
+    }
+
+    private boolean isTargetKilled() {
+        if (this.target.hasPower("Minion")) {
+            return false;
+        }
+
+        AbstractMonster monster = (AbstractMonster) this.target;
+        boolean isDead = monster.isDying || monster.currentHealth <= 0;
+
+        // TODO: SlimeBoss should use halfdead state instead of disappearing until the next turn
+        boolean canBypassHalfDead = this.target instanceof BGDarkling ||
+                                     this.target instanceof BGAwakenedOne;
+        boolean isActuallyDead = !this.target.halfDead || canBypassHalfDead;
+
+        return isDead && isActuallyDead;
+    }
+
+    private void grantStrengthToPlayer() {
+        // TODO degraffa: Use BG StrengthPower
+        addToBot(
+            new ApplyPowerAction(
+                AbstractDungeon.player,
+                AbstractDungeon.player,
+                new StrengthPower(AbstractDungeon.player, this.strengthGain),
+                this.strengthGain
+            )
+        );
     }
 }
