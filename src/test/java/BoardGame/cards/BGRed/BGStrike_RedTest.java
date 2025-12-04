aac3b6881938fb1e31ef81b37125d289e9e52f4e
degraffa <ard96@cornell.edu>
2025-12-03 20:46:05 -0500

    Remove tests, update mod config files

--- src/test/java/BoardGame/cards/BGRed/BGStrike_RedTest.java
+++ src/test/java/BoardGame/cards/BGRed/BGStrike_RedTest.java
@@ -1,5 +1,9 @@
 package BoardGame.cards.BGRed;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
@@ -13,10 +17,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
 
 /**
  * Tests for BGStrike_Red card.
@@ -76,127 +76,127 @@ class BGStrike_RedTest {
         card = new BGStrike_Red();
     }
 
-    @Test
-    void testCardInitialization_HasCorrectID() {
-        assertThat(card.cardID).isEqualTo("BGStrike_R");
-    }
+    // @Test
+    // void testCardInitialization_HasCorrectID() {
+    //     assertThat(card.cardID).isEqualTo("BGStrike_R");
+    // }
 
-    @Test
-    void testCardInitialization_HasBaseDamageOfOne() {
-        assertThat(card.baseDamage).isEqualTo(1);
-    }
+    // @Test
+    // void testCardInitialization_HasBaseDamageOfOne() {
+    //     assertThat(card.baseDamage).isEqualTo(1);
+    // }
 
-    @Test
-    void testCardInitialization_CostsOneEnergy() {
-        assertThat(card.cost).isEqualTo(1);
-    }
+    // @Test
+    // void testCardInitialization_CostsOneEnergy() {
+    //     assertThat(card.cost).isEqualTo(1);
+    // }
 
-    @Test
-    void testCardInitialization_IsAttackType() {
-        assertThat(card.type).isEqualTo(AbstractCard.CardType.ATTACK);
-    }
+    // @Test
+    // void testCardInitialization_IsAttackType() {
+    //     assertThat(card.type).isEqualTo(AbstractCard.CardType.ATTACK);
+    // }
 
-    @Test
-    void testCardInitialization_IsBasicRarity() {
-        assertThat(card.rarity).isEqualTo(AbstractCard.CardRarity.BASIC);
-    }
+    // @Test
+    // void testCardInitialization_IsBasicRarity() {
+    //     assertThat(card.rarity).isEqualTo(AbstractCard.CardRarity.BASIC);
+    // }
 
-    @Test
-    void testCardInitialization_TargetsEnemy() {
-        assertThat(card.target).isEqualTo(AbstractCard.CardTarget.ENEMY);
-    }
+    // @Test
+    // void testCardInitialization_TargetsEnemy() {
+    //     assertThat(card.target).isEqualTo(AbstractCard.CardTarget.ENEMY);
+    // }
 
-    @Test
-    void testCardTags_ContainsStrikeTag() {
-        assertThat(card.tags).contains(AbstractCard.CardTags.STRIKE);
-    }
+    // @Test
+    // void testCardTags_ContainsStrikeTag() {
+    //     assertThat(card.tags).contains(AbstractCard.CardTags.STRIKE);
+    // }
 
-    @Test
-    void testCardTags_ContainsStarterStrikeTag() {
-        assertThat(card.tags).contains(AbstractCard.CardTags.STARTER_STRIKE);
-    }
+    // @Test
+    // void testCardTags_ContainsStarterStrikeTag() {
+    //     assertThat(card.tags).contains(AbstractCard.CardTags.STARTER_STRIKE);
+    // }
 
-    @Test
-    void testUpgrade_StartsNotUpgraded() {
-        assertThat(card.upgraded).isFalse();
-    }
+    // @Test
+    // void testUpgrade_StartsNotUpgraded() {
+    //     assertThat(card.upgraded).isFalse();
+    // }
 
-    @Test
-    void testUpgrade_SetsUpgradedFlagToTrue() {
-        card.upgrade();
+    // @Test
+    // void testUpgrade_SetsUpgradedFlagToTrue() {
+    //     card.upgrade();
 
-        assertThat(card.upgraded).isTrue();
-    }
+    //     assertThat(card.upgraded).isTrue();
+    // }
 
-    @Test
-    void testUpgrade_IncreasesBaseDamageByOne() {
-        int originalDamage = card.baseDamage;
+    // @Test
+    // void testUpgrade_IncreasesBaseDamageByOne() {
+    //     int originalDamage = card.baseDamage;
 
-        card.upgrade();
+    //     card.upgrade();
 
-        assertThat(card.baseDamage).isEqualTo(originalDamage + 1);
-        assertThat(card.baseDamage).isEqualTo(2);
-    }
+    //     assertThat(card.baseDamage).isEqualTo(originalDamage + 1);
+    //     assertThat(card.baseDamage).isEqualTo(2);
+    // }
 
-    @Test
-    void testUpgrade_OnlyWorksOnce() {
-        card.upgrade();
-        int damageAfterFirstUpgrade = card.baseDamage;
+    // @Test
+    // void testUpgrade_OnlyWorksOnce() {
+    //     card.upgrade();
+    //     int damageAfterFirstUpgrade = card.baseDamage;
 
-        card.upgrade();
+    //     card.upgrade();
 
-        assertThat(card.baseDamage).isEqualTo(damageAfterFirstUpgrade);
-        assertThat(card.upgraded).isTrue();
-    }
+    //     assertThat(card.baseDamage).isEqualTo(damageAfterFirstUpgrade);
+    //     assertThat(card.upgraded).isTrue();
+    // }
 
-    @Test
-    void testMakeCopy_CreatesNewInstance() {
-        AbstractCard copy = card.makeCopy();
+    // @Test
+    // void testMakeCopy_CreatesNewInstance() {
+    //     AbstractCard copy = card.makeCopy();
 
-        assertThat(copy).isNotSameAs(card);
-    }
+    //     assertThat(copy).isNotSameAs(card);
+    // }
 
-    @Test
-    void testMakeCopy_CreatesCorrectType() {
-        AbstractCard copy = card.makeCopy();
+    // @Test
+    // void testMakeCopy_CreatesCorrectType() {
+    //     AbstractCard copy = card.makeCopy();
 
-        assertThat(copy).isInstanceOf(BGStrike_Red.class);
-    }
+    //     assertThat(copy).isInstanceOf(BGStrike_Red.class);
+    // }
 
-    @Test
-    void testMakeCopy_HasSameCardID() {
-        AbstractCard copy = card.makeCopy();
+    // @Test
+    // void testMakeCopy_HasSameCardID() {
+    //     AbstractCard copy = card.makeCopy();
 
-        assertThat(copy.cardID).isEqualTo(card.cardID);
-    }
+    //     assertThat(copy.cardID).isEqualTo(card.cardID);
+    // }
 
-    @Test
-    void testMakeCopy_CopyIsNotUpgraded() {
-        card.upgrade();
-        AbstractCard copy = card.makeCopy();
+    // @Test
+    // void testMakeCopy_CopyIsNotUpgraded() {
+    //     card.upgrade();
+    //     AbstractCard copy = card.makeCopy();
 
-        assertThat(copy.upgraded).isFalse();
-        assertThat(copy.baseDamage).isEqualTo(1);
-    }
+    //     assertThat(copy.upgraded).isFalse();
+    //     assertThat(copy.baseDamage).isEqualTo(1);
+    // }
 
-    @Test
-    void testUpgradedCard_HasCorrectDamageValue() {
-        BGStrike_Red upgradedCard = new BGStrike_Red();
-        upgradedCard.upgrade();
+    // @Test
+    // void testUpgradedCard_HasCorrectDamageValue() {
+    //     BGStrike_Red upgradedCard = new BGStrike_Red();
+    //     upgradedCard.upgrade();
 
-        assertThat(upgradedCard.baseDamage).isEqualTo(2);
-    }
+    //     assertThat(upgradedCard.baseDamage).isEqualTo(2);
+    // }
 
-    @Test
-    void testMultipleCards_AreIndependent() {
-        BGStrike_Red card1 = new BGStrike_Red();
-        BGStrike_Red card2 = new BGStrike_Red();
+    // @Test
+    // void testMultipleCards_AreIndependent() {
+    //     BGStrike_Red card1 = new BGStrike_Red();
+    //     BGStrike_Red card2 = new BGStrike_Red();
 
-        card1.upgrade();
+    //     card1.upgrade();
 
-        assertThat(card1.upgraded).isTrue();
-        assertThat(card2.upgraded).isFalse();
-        assertThat(card1.baseDamage).isEqualTo(2);
-        assertThat(card2.baseDamage).isEqualTo(1);
-    }
+    //     assertThat(card1.upgraded).isTrue();
+    //     assertThat(card2.upgraded).isFalse();
+    //     assertThat(card1.baseDamage).isEqualTo(2);
+    //     assertThat(card2.baseDamage).isEqualTo(1);
+    // }
 }
